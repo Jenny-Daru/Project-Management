c74a73b830b8a064c4f1b59c1fb798c8361a387f
jenny <dooly3392@gmail.com>
2024-02-09 00:57:07 +0900

    <style>: TailwindCSS로 모달 스타일링 <feat>: NewProject.jsx에서 Cancel 버튼 구현하기

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,22 +1,29 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
-import { createPortal } from "react-dom"
+import { createPortal } from "react-dom";
+import Button from "./Button.jsx";
 
-const Modal = forwardRef(function Modal({children, buttonCaption}, ref) {
-    const dialog = useRef();
-    useImperativeHandle(ref, () => {
-        return {
-            open() {
-                dialog.current.showModal();
-            }
-        };
-    });
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
 
-    return createPortal (<dialog ref={dialog}>
-        {children}
-        <form method="dialog">
-            <button>{buttonCaption}</button>
-        </form>
-    </dialog>, document.getElementById('modal-root'));
+  return createPortal(
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+    >
+      {children}
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
+      </form>
+    </dialog>,
+    document.getElementById("modal-root")
+  );
 });
 
 export default Modal;
